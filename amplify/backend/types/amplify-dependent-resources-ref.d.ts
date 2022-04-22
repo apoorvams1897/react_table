cb69042d3538126ad6b64fcc15d869126ac1874d
apoorvams1897 <60228910+apoorvams1897@users.noreply.github.com>
2022-04-22 18:22:05 +0530

    Add files via upload

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reacttablef75e1eef": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
